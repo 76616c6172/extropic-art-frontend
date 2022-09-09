79ddb409fdb0c70e93647259cfa5afd87273490d
valar <68338647+76616c6172@users.noreply.github.com>
2022-09-08 20:34:57 -0700

    New: Added navbar mockup

---
+++ src/navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <header className="Navbar-header">
+        <p>
+          Exia <code>home</code>
+        </p>
+      </header>
+    </div>
+
+  );
+}
+
+export default Navbar;
