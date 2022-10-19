fa49611936df0711ee3bdafa77ecd40036246ba8
valar <68338647+76616c6172@users.noreply.github.com>
2022-10-19 16:16:22 -0700

    refactor(Gallery): separate elements from component

--- src/components/gallery/component.tsx
+++ src/components/gallery/component.tsx
@@ -1,7 +1,7 @@
 // CONTAINS THE MAIN EXPORTED REACT COMPONENT OF THIS FOLDER WITH THE SAME NAME
 // TODO: CLEAN THIS CODE
 
-import React, { useState, useEffect, SetStateAction, useContext } from 'react'
+import React, { useState, useEffect, SetStateAction, useContext, useReducer } from 'react'
 import { Waypoint } from "react-waypoint"
 import { useAxios } from "./funcs"
 import { Delay } from "./funcs"
@@ -9,80 +9,165 @@ import { TEST } from "../../index" //testing importing "global state variable"
 import axios from 'axios'
 import { prototype } from 'events'
 
+import { GALLERY_IMAGE } from './elements'
+
 const URL = "https://exia.art/api/1"
 const MS_TIME_BETWEEN_REFRESH = 5000
 
-var DID_SET_NEWEST_JOB = false
-var IS_FIRST_PAGE_LOAD = true
-var RECEIVED_DATA_FROM_QUEUE_API = false
 var LIVE_NEWEST_COMPLETED_JOB: string = ""
 var OLD_NEWEST_COMPLETED_JOB: any = null
+
+var LIVE_GAL_NOT_FIRED_LOOP = true
+var GAL_NOT_FIRED_LOOP = true
+
+var DID_SET_NEWEST_JOB = false
+
 var COUNTER = 1
 
-// Emits one gallery image job_object
-export function GALLERY_IMAGE(props: any) {
-  const IMG_API_URL = "https://exia.art/api/0/img?type=full?jobid="
-  const JOB_URL = "https://exia.art/api/0/jobs?jobid="
+// Experimental
+var PUSH_NEW_JOBS = false
+// Declare the gallery component that autoupdates with new completed jobs above the scrolling one
+// To add images above the infinite gallery, simply add jobs to this array
+function Live_Gallery(): JSX.Element {
 
-  // Set up reactive variable jobMetadData for use by each gallery image element
-  const [jobMetaData, setJobMetaData] = useState(
-    {
-      "jobid": "0", "prompt": "loading..", "job_status": "completed", "iteration_status": 0, "iteration_max": 250, "img_path": "https://exia.art/api/0/img?jobid=0"
-    })
+  var DID_SET_NEWEST_JOB = false
+
+  const [newestJob, setNewestJob] = useState(0)
+  // Asynchrounously makes requests to check the live job queue over and over
+  const continouslyRefreshJobQueue = async () => {
+    while (true) {
+      axios.get(URL + "/status").then((result) => {
+        OLD_NEWEST_COMPLETED_JOB = LIVE_NEWEST_COMPLETED_JOB
+        LIVE_NEWEST_COMPLETED_JOB = result.data.newest_completed_job
+
+        if (!DID_SET_NEWEST_JOB) {
+          setNewestJob(parseInt(LIVE_NEWEST_COMPLETED_JOB))
+          DID_SET_NEWEST_JOB = true
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+      await Delay(MS_TIME_BETWEEN_REFRESH)
+    }
+  }
+  // Fire the live queue request loop only once on page load
   useEffect(() => {
-    axios.get(JOB_URL + props.jobid).then(resp => {
-      setJobMetaData(resp.data)
-    })
-  }, []); //empty array as second argument so the request only fires once per element
-  // if this is ommited react will fire this every time GALLERY_IMAGE is rerendered causing an infinite loop 
+    if (LIVE_GAL_NOT_FIRED_LOOP) {
+      continouslyRefreshJobQueue()
+      LIVE_GAL_NOT_FIRED_LOOP = false
+      // set NEWEST_COMPLETED_JOB_ON_FIRST_LOAD
+      //OLD_RESPONSE = response?.data
+    }
+    return
+  }, []) // Empty array so this only happens on first page load and not every time the component reloads
 
-  //opacity-50 hover:opacity-100 
-  return (
-    <div className="opacity-80 hover:opacity-100" >
+  // BIG
+  // Image list related
+  const [dynamicImgList, setDyanmicImgList] = useState([
+    {
+      id: 0, //starting valuej
+      jobid: "0", //placeholder
+    },
+  ])
 
-      <div className='bg-black rounded 
-      shadow-xl  shadow-[#db5481]/25 '
-      >
 
 
-        <a href={IMG_API_URL + props.jobid} target="_blank" rel="noopener noreferrer">
-          <img className='mx-auto justify-center
-          hover:cursor-pointer'
-            /* onClick={() => alert(props.jobid)} */
-            src={IMG_API_URL + props.jobid} />
-        </a>
+  // takes i the jobid of a newly completed job and pushes it to the live gallery
+  const update_img_list = (i: string) => {
+    const new_list = dynamicImgList
+    new_list.push(
+      {
+        id: COUNTER,
+        jobid: i
+      },
+    )
+    setDyanmicImgList(new_list)
+    COUNTER++
+  }
 
-        <div className="px-1 py-1 rounded
-shadow-xl  shadow-[#db5481]/10
-          bg-black"> <Prompt job={jobMetaData} />
-        </div>
 
+
+  // Experiment update the image list
+  if (PUSH_NEW_JOBS) {
+    if (DID_SET_NEWEST_JOB && LIVE_NEWEST_COMPLETED_JOB != OLD_NEWEST_COMPLETED_JOB) {
+      PUSH_NEW_JOBS = true
+      // Push the difference in jobids
+      const live_newest_completed_job = parseInt(LIVE_NEWEST_COMPLETED_JOB)
+      const old_newest_completed_job = parseInt(OLD_NEWEST_COMPLETED_JOB)
+      const number_of_new_completed_jobs = live_newest_completed_job - old_newest_completed_job
+      //build the array to be pushed to the live gallery
+      // Push it
+      for (var i = live_newest_completed_job; i >= old_newest_completed_job; i--) {
+        update_img_list(i.toString())
+        console.log("pushed jobd:", i)
+      }
+    }
+
+
+
+
+    /*
+        const List_element = dynamicImgList.slice(0).reverse().map((job_object: any) => (
+          <React.Fragment key={job_object.id}>
+            < GALLERY_IMAGE jobid={job_object.jobid} />
+          </React.Fragment>
+        ))
+        */
+  }
+
+  // Only render this element when new jobs move "down" from the queue
+  if (dynamicImgList.length > 0) {
+    return (
+      <div className="">
+        {dynamicImgList.slice(0).reverse().map((job_object: any) => (
+          <React.Fragment key={job_object.id}>
+            < GALLERY_IMAGE jobid={job_object.jobid} />
+
+          </React.Fragment>
+        ))}
       </div>
-
-      <div className="py-4 bg-transparent"></div>
-    </div >
-  )
+    )
+  }
+  return (
+    <div>
+      <React.Fragment></React.Fragment>
+    </div>)
 }
 
+export default function GALLERY() {
 
+  const [newestJobOnFirstLoad, setNewestJobOnFirstLoad] = useState(0)
+  // Asynchrounously makes requests to check the live job queue over and over
+  const continouslyRefreshJobQueue = async () => {
+    while (true) {
+      axios.get(URL + "/status").then((result) => {
+        OLD_NEWEST_COMPLETED_JOB = LIVE_NEWEST_COMPLETED_JOB
+        LIVE_NEWEST_COMPLETED_JOB = result.data.newest_completed_job
 
-// Prompt component for each gallery image
-function Prompt(props: any) {
-  return <div className="text-center
-  ">{props.job.prompt}</div>
-}
+        if (!DID_SET_NEWEST_JOB) {
 
-export default function Gallery() {
+          //[NEWEST_COMPLETED_JOB_ON_FIRST_LOAD, setNewestJobOnFirstLoad] = useEffect(parseInt(LIVE_NEWEST_COMPLETED_JOB))
+          setNewestJobOnFirstLoad(parseInt(LIVE_NEWEST_COMPLETED_JOB))
 
-  var [NEWEST_COMPLETED_JOB_ON_FIRST_LOAD, setNewesstJobOnFirstLoad] = useState(0)
+          DID_SET_NEWEST_JOB = true
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+      await Delay(MS_TIME_BETWEEN_REFRESH)
+    }
+  }
+  // Fire the live queue request loop only once on page load
+  useEffect(() => {
+    if (GAL_NOT_FIRED_LOOP) {
+      continouslyRefreshJobQueue()
+      GAL_NOT_FIRED_LOOP = false
+      // set NEWEST_COMPLETED_JOB_ON_FIRST_LOAD
+      //OLD_RESPONSE = response?.data
+    }
+    return
+  }, []) // Empty array so this only happens on first page load and not every time the component reloads
 
-
-
-  //OLD_RESPONSE = response?.data[2]
-
-
-  //  if (OLD_RESPONSE.data != response?.data) {
-  //  }
 
 
   // Define an array of "job objects" that is then used to build the gallery
@@ -91,7 +176,7 @@ export default function Gallery() {
   const gallery_items_list = [
     {
       id: 1,
-      jobid: String(NEWEST_COMPLETED_JOB_ON_FIRST_LOAD),
+      jobid: String(newestJobOnFirstLoad),
     },
   ]
 
@@ -99,9 +184,8 @@ export default function Gallery() {
   // Build out the full list of completed job objects based on the latest completed job
   // This will have to be changed to not build all the way down to job 1
   // But rather depend on how far the user has scrolled, else it won't scale to 1000x jobs
-
   let id = 2
-  for (let i = NEWEST_COMPLETED_JOB_ON_FIRST_LOAD - 1; i > 0; i--) {
+  for (let i = newestJobOnFirstLoad - 1; i > 0; i--) {
     gallery_items_list.push({ id: id, jobid: String(i) })
     id++
   }
@@ -113,10 +197,14 @@ export default function Gallery() {
   }
 
   // Declare the ininite scrolling gallery
-  const INFINITE_GALLERY = (props: any) => {
+  const Infinite_Gallery = (props: any) => {
     const limit = 3
     const [page, setPage] = useState(1);
     const rendered_image_list = useInfiniteScroll(gallery_items_list, limit, page);
+
+
+
+
 
     return (
       <div className="">
@@ -132,136 +220,10 @@ export default function Gallery() {
   }
 
 
-  let [dynamicImgList, setDyanmicImgList] = useState([
-    {
-      id: 0, //starting valuej
-      jobid: "0", //placeholder
-    },
-  ])
-
-
-
-  // Declare the gallery component that autoupdates with new completed jobs above the scrolling one
-  // To add images above the infinite gallery, simply add jobs to this array
-  const LIVE_GALLERY = () => {
-
-    // Set up custom Axios hook for making API requests to the api/0/queue endpoint
-    const { response, loading, error, sendRequest } = useAxios({
-      method: "GET",
-      url: `${URL}/queue`,
-      headers: {
-        accept: '*/*'
-      }
-    })
-
-    // maps an array [ {jobid: 300}, {jobid: 200} ] and adds all items to the live gallery
-    const update_img_list = (i: string) => {
-      const new_list = dynamicImgList
-      new_list.push(
-        {
-          id: COUNTER,
-          jobid: i
-        },
-      )
-      setDyanmicImgList(new_list)
-      COUNTER++
-    }
-
-    // Asynchrounously makes requests to check the live job queue over and over
-    const continouslyRefreshJobQueue = async () => {
-      while (true) {
-        sendRequest()
-        axios.get(URL + "/status").then((result) => {
-          OLD_NEWEST_COMPLETED_JOB = LIVE_NEWEST_COMPLETED_JOB
-          LIVE_NEWEST_COMPLETED_JOB = result.data.newest_completed_job
-
-          //console.log(LIVE_NEWEST_COMPLETED_JOB)
-
-          if (DID_SET_NEWEST_JOB && LIVE_NEWEST_COMPLETED_JOB != OLD_NEWEST_COMPLETED_JOB) {
-            // Push the difference in jobids
-            const live_newest_completed_job = parseInt(LIVE_NEWEST_COMPLETED_JOB)
-            const old_newest_completed_job = parseInt(OLD_NEWEST_COMPLETED_JOB)
-            const number_of_new_completed_jobs = live_newest_completed_job - old_newest_completed_job
-
-            //build the array to be pushed to the live gallery
-
-            // Push it
-            for (var i = live_newest_completed_job; i > old_newest_completed_job; i--) {
-              update_img_list(i.toString())
-              console.log("pushed jobd:", i)
-            }
-          }
-
-
-
-
-
-
-
-
-          if (!DID_SET_NEWEST_JOB) {
-            setNewesstJobOnFirstLoad(parseInt(LIVE_NEWEST_COMPLETED_JOB))
-            DID_SET_NEWEST_JOB = true
-          }
-
-        }).catch(err => {
-          console.log(err)
-        })
-        await Delay(MS_TIME_BETWEEN_REFRESH)
-      }
-    }
-    // Fire the live queue request loop only once on page load
-    useEffect(() => {
-      if (IS_FIRST_PAGE_LOAD) {
-        continouslyRefreshJobQueue()
-        IS_FIRST_PAGE_LOAD = false
-        // set NEWEST_COMPLETED_JOB_ON_FIRST_LOAD
-        //OLD_RESPONSE = response?.data
-      }
-      return
-    }, []) // Empty array so this only happens on first page load and not every time the component reloads
-
-    /*
-    /// check the first image list item
-    if (dynamicImgList[0].jobid == "0" || RECEIVED_DATA_FROM_QUEUE_API) {
-      if (response?.data.length > 0) {
-        //console.log("response has length greater 1")
-        //console.log("saving response data")
-        console.log(response?.data)
-        OLD_RESPONSE = response?.data
-        RECEIVED_DATA_FROM_QUEUE_API = true
-      }
-    }
-      */
-
-    const List_element = dynamicImgList.slice(0).reverse().map((job_object: any) => (
-      <React.Fragment key={job_object.id}>
-        < GALLERY_IMAGE jobid={job_object.jobid} />
-      </React.Fragment>
-    ))
-
-
-    // Only render this element when new jobs move "down" from the queue
-    if (dynamicImgList.length > 0) {
-      return (
-        <div className="">
-          {dynamicImgList.slice(0).reverse().map((job_object: any) => (
-            <React.Fragment key={job_object.id}>
-              < GALLERY_IMAGE jobid={job_object.jobid} />
-
-            </React.Fragment>
-          ))}
-        </div>
-      )
-    } else {
-      return (<div></div>)
-    }
-  }
-
   return (
     <div>
-      < LIVE_GALLERY />
-      < INFINITE_GALLERY />
+      < Live_Gallery />
+      < Infinite_Gallery />
     </div>
   );
 }
---
+++ src/components/gallery/elements.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, SetStateAction, useContext, useReducer } from 'react'
+import axios from 'axios'
+
+// Emits one gallery image element, displaying both the picture and the corresponding prompt
+// TODO: should display seed too!
+// Prompt component for each gallery image
+function Prompt(props: any) {
+	return <div className="text-center
+  ">{props.job.prompt}</div>
+}
+
+export function GALLERY_IMAGE(props: any) {
+	const IMG_API_URL = "https://exia.art/api/0/img?type=full?jobid="
+	const JOB_URL = "https://exia.art/api/0/jobs?jobid="
+
+	// Set up reactive variable jobMetadData for use by each gallery image element
+	const [jobMetaData, setJobMetaData] = useState(
+		{
+			"jobid": "0", "prompt": "loading..", "job_status": "completed", "iteration_status": 0, "iteration_max": 250, "img_path": "https://exia.art/api/0/img?jobid=0"
+		})
+	useEffect(() => {
+		axios.get(JOB_URL + props.jobid).then(resp => {
+			setJobMetaData(resp.data)
+		})
+	}, []); //empty array as second argument so the request only fires once per element
+	// if this is ommited react will fire this every time GALLERY_IMAGE is rerendered causing an infinite loop 
+
+	//opacity-50 hover:opacity-100 
+	return (
+		<div className="opacity-80 hover:opacity-100" >
+
+			<div className='bg-black rounded 
+      shadow-xl  shadow-[#db5481]/25 '
+			>
+
+				<a href={IMG_API_URL + props.jobid} target="_blank" rel="noopener noreferrer">
+					<img className='mx-auto justify-center
+          hover:cursor-pointer'
+						/* onClick={() => alert(props.jobid)} */
+						src={IMG_API_URL + props.jobid} />
+				</a>
+
+				<div className="px-1 py-1 rounded
+          shadow-xl  shadow-[#db5481]/10
+          bg-black"> <Prompt job={jobMetaData} />
+				</div>
+
+			</div>
+
+			<div className="py-4 bg-transparent"></div>
+		</div >
+	)
+}
