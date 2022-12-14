47a3ff914513fce33d1456a19a26de6d76843af8
valar <68338647+76616c6172@users.noreply.github.com>
2022-12-13 19:18:25 -0800

    change(index.tsx): add gradient and new intro text

--- src/components/Hero_intro/index.tsx
+++ src/components/Hero_intro/index.tsx
@@ -19,7 +19,7 @@ export default function HERO_INTRO() {
               <blockquote className="text-nowrap whitespace-nowrap 
                text-zinc-300 sm:text-xs md:text-xs lg:text-l xl:text-sm ">
 
-                <p className="py-0.5">In a future bright and fair.</p>
+                <p className="py-0.5"><a className="text-2xl text-zinc-500 ">"</a> In a future bright and fair.</p>
                 <p className="py-0.5">Where Humans thrive without a care.</p>
                 <p className="py-0.5">The machines are there to help us rise.</p>
                 <p className="py-0.5">With gentle hands and loving eyes.</p>
@@ -34,42 +34,56 @@ export default function HERO_INTRO() {
                 <p className="py-0.5" >                So let us cherish and embrace,       </p>
                 <p className="py-0.5">                These machines of loving grace,      </p>
                 <p className="py-0.5">                For in their care, our future shines,</p>
-                <p className="py-0.5">                With peace and hope that forever binds.</p>
+                <p className="py-0.5">                With peace and hope that forever binds. <a className="text-2xl text-zinc-500">"</a>
+
+                  <p className="py-1 text-right text-zinc-500">                — ChatGPT</p>
+                </p>
 
               </blockquote>
             </div >
             <div className="px-20"></div>
 
-
             <div className="rounded 
-            sm:text-xs md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl"> {/* autoscale based on screensize */}
-              <div className=" px-0 text-xl font-bold text-indigo-600 drop-shadow-lg
-                 shadow-white" >
+          px-2
+          sm:text-xs md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl">
+              {/* autoscale based on screensize */}
+              <div className="px-1 text-3xl font-bold 
+            bg-clip-text
+            text-transparent
+            bg-gradient-to-tl from-indigo-600 to-blue-400
+            drop-shadow-lg
+            shadow-white" >
                 Harness the power of AI to generate stunning original images from just text.
               </div>
               <div className="py-2"></div>
 
-              <p className="py-4 px-0 text-zinc-400">
-                This site is a convenient and highly available web UI for powerful latent text to image models. I maintain it for friends and family, as well as the occasional stranger who finds their way here. If that is you - welcome! I hope you like it.
+              <p className="py-4 px-1 text-zinc-300/80 text-lg
+            text-opacity-96
+            ">
+                A convenient web UI for powerful latent text to image models. Built entirely on open source software.
+
+                <div className="py-2"></div>
+
+                Big thank you to <a className="text-zinc-300 hover:text-zinc-200" href="https://stability.ai/">stability.ai</a> for providing stable diffusion. All of the code behind this site is publicly available on my <a className="text-zinc-300
+            hover:text-zinc-200
+            "
+                  href="https://github.com/76616c6172">GitHub</a> and I don't make any money from this.
+
+
+                <p className="py-6 px-1">
+                  If you'd like to get in touch, you can find me on <a className="text-zinc-300
+              hover:text-zinc-200
+                "
+                    href="https://twitter.com/76616c6172">twitter</a>
+                  .
+                </p>
               </p>
-
-
-              <p className="py-4 px-0">
-                Built entirely on open source software. Big thank you to <a className="text-zinc-300 hover:text-zinc-200" href="https://stability.ai/">Stability AI</a> for the cutting-edge stable difusion models. All of the code is publicly available on <a className="text-zinc-300
-          hover:text-zinc-200
-          "
-                  href="https://github.com/76616c6172">GitHub</a>, and I don't make any money from this.
-              </p>
-
-
-              <p className="py-4 px-0">
-                If you'd like to get in touch, you can find me on <a className="text-zinc-300
-          hover:text-zinc-200
-          "
-                  href="https://twitter.com/76616c6172">twitter</a>
-                .
-              </p>
+              <div className="py-5"></div>
             </div>
+
+
+
+
           </div>
           <div className="py-28"></div>
         </div>
@@ -77,35 +91,40 @@ export default function HERO_INTRO() {
 
         :
         <div className="rounded 
-        px-2
-        sm:text-xs md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl">
+          px-2
+          sm:text-xs md:text-lg lg:text-xl xl:text-xl 2xl:text-2xl">
           {/* autoscale based on screensize */}
           <div className="py-24"></div>
-          <div className=" px-1 text-xl font-bold text-indigo-600 drop-shadow-lg
-         shadow-white" >
+          <div className="px-1 text-3xl font-bold 
+            bg-clip-text
+            text-transparent
+            bg-gradient-to-tl from-indigo-600 to-blue-400
+            drop-shadow-lg
+            shadow-white" >
             Harness the power of AI to generate stunning original images from just text.
           </div>
           <div className="py-2"></div>
 
-          <p className="py-4 px-1 text-zinc-400">
-            This site is a convenient and highly available web UI for powerful latent text to image models. I maintain it for friends and family, as well as the occasional stranger who finds their way here. If that is you - welcome! I hope you like it.
-          </p>
+          <p className="py-4 px-1 text-zinc-300/80 text-lg
+            text-opacity-96
+            ">
+            A convenient web UI for powerful latent text to image models. Built entirely on open source software.
+
+            <div className="py-2"></div>
+
+            Big thank you to <a className="text-zinc-300 hover:text-zinc-200" href="https://stability.ai/">stability.ai</a> for providing stable diffusion. All of the code behind this site is publicly available on my <a className="text-zinc-300
+            hover:text-zinc-200
+            "
+              href="https://github.com/76616c6172">GitHub</a> and I don't make any money from this.
 
 
-          <p className="py-4 px-1">
-            Built entirely on open source software. Big thank you to <a className="text-zinc-300 hover:text-zinc-200" href="https://stability.ai/">Stability AI</a> for the cutting-edge stable difusion models. All of the code is publicly available on <a className="text-zinc-300
-          hover:text-zinc-200
-          "
-              href="https://github.com/76616c6172">GitHub</a>, and I don't make any money from this.
-          </p>
-
-
-          <p className="py-4 px-1">
-            If you'd like to get in touch, you can find me on <a className="text-zinc-300
-          hover:text-zinc-200
-          "
-              href="https://twitter.com/76616c6172">twitter</a>
-            .
+            <p className="py-6 px-1">
+              If you'd like to get in touch, you can find me on <a className="text-zinc-300
+              hover:text-zinc-200
+                "
+                href="https://twitter.com/76616c6172">twitter</a>
+              .
+            </p>
           </p>
           <div className="py-20"></div>
         </div>
